0a692878a68c4690189b79e7a4269761278e595c
Brian LeRoux <brian@westcoastlogic.com>
2013-10-12 12:58:14 -0700

    rework to browserify

---
+++ gruntfile.js
@@ -0,0 +1,48 @@
+var shell = require('shelljs')
+,   harp  = require('harp')
+,   path  = require('path')
+
+module.exports = function(grunt) {
+    
+    // init ceremonies
+    //
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserify: {
+            'www/x.js': ['lib/index.js']
+        },
+
+        watch: {
+            files: ["lib/**/*.js", 'test/**/*.js'],
+            tasks: ['browserify', 'test']
+        },
+
+        concurrent: {
+            target: { 
+                tasks:['serve', 'watch'],
+                options:{logConcurrentOutput: true}
+            }
+        }
+    })
+    
+    grunt.loadNpmTasks('grunt-browserify')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-concurrent')
+
+    // custom business 
+    //
+    grunt.registerTask('default', ['concurrent:target'])
+    
+    grunt.registerTask('test', 'Run the unit tests.', function() {
+        shell.exec('npm test')
+    })
+
+    grunt.registerTask('serve', 'Serve www with Harp.', function() {
+        var done = this.async()
+        var projectPath = path.join(__dirname, 'www')
+        harp.server(projectPath, [], function cb() {
+            grunt.log.write("\nHarp server is now running at http://localhost:9966\n\n")
+        })        
+    })
+}
--- index.js
+++
@@ -1,7 +0,0 @@
-var map = L.mapbox.map('map', 'examples.map-y7l23tes').setView([37.9, -77], 5);
-
-
-document.getElementById('slide-menu-button').onclick = function(e) {
-    var cl = document.body.classList
-    cl.contains('left-nav') ? cl.remove('left-nav') : cl.add('left-nav')
-}
